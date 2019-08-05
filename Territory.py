3000375abd1dbedc8b972c1f49d281c3223e1b83
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-05 19:31:24 +0200

    Remove unused code

--- Territory.py
+++ Territory.py
@@ -5,18 +5,10 @@ from Color import Color
 class Territory:
 
     def __init__(self):
-        self.color = Color(3)
+        self.color = Color.Grey
         self.troops = 0
         self.neighbours = list()
         return
 
     def set_neighbours(self,territory):
         self.neighbours.extend(territory)
-
-
-    # def in_egypt(cls):
-    #     return
-    #
-    # @classmethod
-    # def in_usa(cls):
-    #     return
