d1534900ed7816340ed978b7b1633fda1678fcef
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-05 19:37:34 +0200

    Update main

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
+from HumanAgent import HumanAgent
 from Game import Game
-from Player import Player
-from Agent import Agent
+from PassiveAgent import PassiveAgent
 from Board import Board
 from Territory import Territory
-# import tkinter as tk
+from Color import Color
 
-# player = Player('blue')
-# agent_1 = Agent('blue')
-# agent_2 = Agent('red')
-#
+player = HumanAgent(Color.Blue)
+agent_1 = PassiveAgent(Color.Blue)
+agent_2 = PassiveAgent(Color.Red)
+
 # game = Game.start_simulation_mode(agent_1, agent_2, 'Egypt')
-# # game = Game.start_playing_mode(player, agent_2, 'USA')
-#
-# player.turn({
-#     1: 1,
-#     2: 2,
-#     3: 1
-# })
-#
-# print(game.board.country_name)
-#
-#
-board = Board.init_usa()
-print(board.map)
-# print(board.map[1])
-# x = open("egypt.txt", "r")
-# s = x.read()
-# print(s[0])
-# root = tk.Tk()
-#
-#
-# def hello_call_back():
-#     print('hello')
-#
-#
-# w = tk.Button(root, text='click me', command=hello_call_back)
-# w.pack()
-# tk.mainloop()
+game = Game.start_playing_mode(player, agent_2, 'Egypt')
+game.board.display()
+
+player.first_play(game, {1: 19, 2: 1})
+game.board.display()
+
+agent_2.first_play(game, {3: 1, 4: 19})
+game.board.display()
+
+player.turn(game, {5: 15, 6: 5})
+game.board.display()
+
+agent_2.turn(game, {7: 10, 8: 10})
+game.board.display()
