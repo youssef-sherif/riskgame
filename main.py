216ed93cd4808ff447854a0ef7824c1d50714cd3
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-07 13:14:03 +0200

    method turn is for testing purposes only and will be removed

--- main.py
+++ main.py
@@ -11,16 +11,11 @@ agent_2 = PassiveAgent(Color.Red)
 
 # game = Game.start_simulation_mode(agent_1, agent_2, 'Egypt')
 game = Game.start_playing_mode(player, agent_2, 'Egypt')
+
+player.turn(game, {1: 1})
 game.board.display()
 
-player.first_play(game, {1: 19, 2: 1})
-game.board.display()
+print(agent_2.color)
+agent_2.turn(game)
 
-agent_2.first_play(game, {3: 1, 4: 19})
-game.board.display()
-
-player.turn(game, {5: 15, 6: 5})
-game.board.display()
-
-agent_2.turn(game, {7: 10, 8: 10})
 game.board.display()
