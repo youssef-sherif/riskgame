81e359f1508f0aea9d5ef1eb0432b565df9d131e
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-07 13:09:18 +0200

    Add id to territory as it would be useful for the client and add to_json methods to Board and Territory to start developing api

--- Territory.py
+++ Territory.py
@@ -4,11 +4,19 @@ from Color import Color
 
 class Territory:
 
-    def __init__(self):
+    def __init__(self, id):
         self.color = Color.Grey
         self.troops = 0
         self.neighbours = list()
+        self.id = id
         return
 
     def set_neighbours(self,territory):
         self.neighbours.extend(territory)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'color': str(self.color),
+            'troops': self.troops
+        }
