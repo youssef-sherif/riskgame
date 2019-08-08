3c9559d3006356aafa750f35130c4324868c857e
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-08 20:53:46 +0200

    Fix spaces

--- PassiveAgent.py
+++ PassiveAgent.py
@@ -4,9 +4,9 @@ from Color import Color
 
 class PassiveAgent(Agent):
 
-    def place_territories(self, board, territory, armies_count):
+    def place_territories(self, board):
+        territory = board.find_territory_with_fewest_armies(self.color)
         if board.map[territory].color == Color.Grey or board.map[territory].color == self.get_opponent_color():
             raise Exception('PassiveAgent can only place armies on own territories and cannot claim neutral territories')
-        self.available_armies_count -= armies_count
-        board.update(territory, armies_count, self.color)
+        board.update(territory, self.available_armies_count, self.color)
 
