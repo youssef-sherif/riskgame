c885f901950542f65cb69c9825d991cd4bb5af66
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-08 02:12:38 +0200

    Remove turn method

--- main.py
+++ main.py
@@ -11,11 +11,4 @@ agent_2 = PassiveAgent(Color.Red)
 
 # game = Game.start_simulation_mode(agent_1, agent_2, 'Egypt')
 game = Game.start_playing_mode(player, agent_2, 'Egypt')
-
-player.turn(game, {1: 1})
-game.board.display()
-
-print(agent_2.color)
-agent_2.turn(game)
-
 game.board.display()
