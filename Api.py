2880f61c5e1844acacaf79f9d9969901efbe8704
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-08 02:08:17 +0200

    Add attack and place_armies endpoints

--- Api.py
+++ Api.py
@@ -14,13 +14,14 @@ class Api:
 
     def __init__(self):
         self.app = Flask(__name__)
+        self.app.add_url_rule('/territories/<territory>/place', 'place_armies', self.place_armies, methods=["POST"])
+        self.app.add_url_rule('/territories/<attacked_territory>/attack', 'attack', self.attack, methods=["POST"])
         self.app.add_url_rule('/game', 'start_playing_game', self.start_playing_game, methods=["GET"])
         self.app.add_url_rule('/simulation', 'start_simulation_game', self.start_simulation_game, methods=["GET"])
-        self.app.add_url_rule('/<id>/blue-neighbours', 'get_blue_neighbours_for_id', self.get_blue_neighbours_for_id, methods=["GET"])
-        self.app.add_url_rule('/<id>/red-neighbours', 'get_red_neighbours_for_id', self.get_red_neighbours_for_id, methods=["GET"])
         self.app.add_url_rule('/neighbours_to_blue', 'get_neighbours_to_blue', self.get_neighbours_to_blue, methods=["GET"])
         self.app.add_url_rule('/neighbours_to_red', 'get_neighbours_to_red', self.get_neighbours_to_red, methods=["GET"])
-        self.app.add_url_rule('/armies', 'place_armies', self.place_armies, methods=["POST"])
+        self.app.add_url_rule('/blue-armies', 'receive_blue_armies', self.receive_blue_armies, methods=["GET"])
+        self.app.add_url_rule('/red-armies', 'receive_red_armies', self.receive_red_armies, methods=["GET"])
         self.game = None
 
     @cross_origin(origin='http://localhost:3000')
@@ -49,34 +50,14 @@ class Api:
         return json_response(self.game.board.to_json())
 
     @cross_origin(origin='http://localhost:3000')
-    def get_blue_neighbours_for_id(self, id):
-        territories = self.game.board.find_territories_with_color(Color.Blue)
-        territory_locations = list()
-        print(self.game.board.map[int(id)].neighbours)
-        for i in territories.keys():
-            if str(i) in self.game.board.map[int(id)].neighbours:
-                territory_locations.append(i)
-
-        return json_response(territory_locations)
-
-    @cross_origin(origin='http://localhost:3000')
-    def get_red_neighbours_for_id(self, id):
-        territories = self.game.board.find_territories_with_color(Color.Red)
-        territory_locations = list()
-        print(self.game.board.map[int(id)].neighbours)
-        for i in territories.keys():
-            if str(i) in self.game.board.map[int(id)].neighbours:
-                territory_locations.append(i)
-
-        return json_response(territory_locations)
-
-    @cross_origin(origin='http://localhost:3000')
     def get_neighbours_to_blue(self):
         blue_territories = self.game.board.find_territories_with_color(Color.Blue)
         neighbours_to_blue = list()
         for blue_territory in blue_territories.values():
             for neighbour in blue_territory.neighbours:
-                neighbours_to_blue  .append(neighbour)
+                if self.game.board.map[int(neighbour)].color is Color.Grey:
+                    continue
+                neighbours_to_blue.append(neighbour)
 
         return json_response(neighbours_to_blue)
 
@@ -86,13 +67,44 @@ class Api:
         neighbours_to_red = list()
         for red_territory in red_territories.values():
             for neighbour in red_territory.neighbours:
+                if self.game.board.map[int(neighbour)].color is Color.Grey:
+                    continue
                 neighbours_to_red.append(neighbour)
 
         return json_response(neighbours_to_red)
 
     @cross_origin(origin='http://localhost:3000')
-    def place_armies(self):
-        return json_response()
+    def place_armies(self, territory):
+        armies_count = int(request.get_json()['armies_count'])
+        self.game.player.place_territories(self.game.board, int(territory), armies_count)
+        json = {
+            'available_armies': self.game.player.available_armies_count,
+            'map': self.game.board.to_json()
+        }
+        return json_response(json)
+
+    @cross_origin(origin='http://localhost:3000')
+    def attack(self, attacked_territory):
+        armies_count = int(request.get_json()['armies_count'])
+        attacking_territory = int(request.get_json()['attacking_territory'])
+        success = self.game.player.attack(self.game.board, int(attacking_territory), int(attacked_territory), armies_count)
+        json = {
+            'map': self.game.board.to_json(),
+            'attack_successful': success
+        }
+        return json_response(json)
+
+    @cross_origin(origin='http://localhost:3000')
+    def receive_blue_armies(self):
+        self.game.player.receive_armies(self.game.board)
+
+        return json_response(self.game.player.available_armies_count)
+
+    @cross_origin(origin='http://localhost:3000')
+    def receive_red_armies(self):
+        self.game.opponent_agent.receive_armies(self.game.board)
+
+        return json_response(self.game.player.available_armies_count)
 
 
 api = Api()
