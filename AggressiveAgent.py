47aa40023061a84e8578b389f4f6262faa7db2d1
Youssef Sherif <youssefsabry97@gmail.com>
2019-08-09 17:55:10 +0200

    Add check for Aggressive agent to raise Exception if no opposite color neighbours

--- AggressiveAgent.py
+++ AggressiveAgent.py
@@ -15,7 +15,11 @@ class AggressiveAgent(Agent):
 
     def attack(self, board):
         attacking_territory = self.find_territory_with_most_armies(board)
-        attacked_territory = self.find_weakest_neighbour_to(board, attacking_territory)
+        try:
+            attacked_territory = self.find_weakest_opposite_color_neighbour_to(board, attacking_territory)
+        except Exception as e:
+            print(e)
+            return False
 
         armies_count = board.map[attacking_territory].troops - 1
 
@@ -30,14 +34,17 @@ class AggressiveAgent(Agent):
 
         return False
 
-    def find_weakest_neighbour_to(self, board, attacking_territory) -> int:
-        print(board.map[attacking_territory].neighbours[0])
+    def find_weakest_opposite_color_neighbour_to(self, board, attacking_territory) -> int:
         territory_number = int(board.map[attacking_territory].neighbours[0])
+        has_opposite_color_neighbour = False
         for i in board.map[attacking_territory].neighbours:
             if board.map[int(i)].color == self.get_opponent_color():
+                has_opposite_color_neighbour = True
                 if board.map[int(i)].troops < board.map[territory_number].troops:
                     territory_number = int(i)
 
+        if not has_opposite_color_neighbour:
+            raise Exception("cannot attack as no opposite color neighbours found")
         return territory_number
 
     def find_territory_with_most_armies(self, board) -> int:
